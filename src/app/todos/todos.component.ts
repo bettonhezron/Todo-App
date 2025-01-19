3c626c75c22028a18f131cee74b25de0ee8bf026
bettonhezron <hezronbet@gmail.com>
2025-01-19 12:46:04 +0300

    add search feature

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -3,16 +3,19 @@ import { TodosService } from '../services/todos.service';
 import { Todo } from '../model/todo.type';
 import { catchError, retry } from 'rxjs';
 import { TodoItemComponent } from '../components/todo-item/todo-item.component';
+import { FormsModule } from '@angular/forms';
+import { FilterTodosPipe } from '../pipes/filter-todos.pipe';
 
 @Component({
   selector: 'app-todos',
-  imports: [TodoItemComponent],
+  imports: [TodoItemComponent, FormsModule, FilterTodosPipe],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
 export class TodosComponent implements OnInit{
 todoService = inject(TodosService);
 todoItems = signal<Array<Todo>> ([]);
+searchTerm = signal('');
 
 ngOnInit(): void {
 this.todoService.getTodosFromApi()
